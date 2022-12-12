2a589ffd68b13e3d9b62db85e5b78f63b19137c9
thimovandermeer <thimovandermeer@gmail.com>
2022-12-12 12:10:40 +0100

    recovering client side changes

--- server/build.gradle.kts
+++ server/build.gradle.kts
@@ -23,6 +23,10 @@ dependencies {
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
     runtimeOnly("com.h2database:h2")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
+    implementation("org.jetbrains.exposed:exposed-core:0.37.3")
+    implementation("org.jetbrains.exposed:exposed-dao:0.37.3")
+    implementation("org.jetbrains.exposed:exposed-jdbc:0.37.3")
+    runtimeOnly("org.postgresql:postgresql")
 }
 
 tasks.withType<KotlinCompile> {
