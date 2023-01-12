946d6b3ad49ce892e16ae1552063f8bc5823a8b5
thimovandermeer <thimovandermeer@gmail.com>
2023-01-12 14:54:27 +0100

    Changed App Icon

--- app/src/main/java/com/example/ft_hangouts/screen/Details/DetailScreen.kt
+++ app/src/main/java/com/example/ft_hangouts/screen/Details/DetailScreen.kt
@@ -1,10 +1,15 @@
 package com.example.ft_hangouts.screen.Details
 
 import android.util.Log
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.modifier.modifierLocalOf
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavHostController
 import com.example.ft_hangouts.ViewModels.DetailsScreenViewModel
@@ -23,7 +28,25 @@ fun DetailsScreen(
         }
     }
     val TAG = "DetailsScreen"
-    Text(text = "Personal details")
     Log.d(TAG, "do we retrieve person info $person")
     Log.d(TAG, "Details = ${details.partnerInfo}")
+    LazyColumn(modifier = Modifier.fillMaxWidth(1f)) {
+        // Add a single item
+        item {
+            Text(text = details.partnerInfo.firstName)
+        }
+        item {
+            Text(text = details.partnerInfo.lastName)
+        }
+        item {
+            Text(text = details.partnerInfo.profession)
+        }
+        item {
+            Text(text = details.partnerInfo.epicBeer)
+        }
+        item {
+            Text(text = details.partnerInfo.favoriteAnimal)
+        }
+    }
+
 }
